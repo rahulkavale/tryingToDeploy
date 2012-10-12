63617e97b080a0bb22de418c9599029dd9c7beb7
Rahul Kavale <kavale.rahul@gmail.com>
2012-10-12 13:28:43 +0530

    Rahul | pushing the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdvision::Application.config.secret_token = 'a7e7bca69a03471f6c6916d0daf8231e86c3ea0219bb95e8a799283ba510c9ba02b008bb030f98b5d16c78725ae44b60b5dbdc64dbd0f4d09a1737f818af5523'
