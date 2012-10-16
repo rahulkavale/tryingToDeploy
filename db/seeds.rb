83c470f4daa0f99dacfda3e6a3fc584c6c89b443
Anay Nayak <anayak@thoughtworks.com>
2012-10-16 17:35:03 +0530

    PROJ-26 (Anay/Rahul) Added seed data for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.delete_all
+Item.create!(:title => 'Coke', :description => 'The soft drink for everyone', :price => 15)
+Item.create!(:title => 'Mac Book pro', :description => 'The ultimate laptop from apple thats fits your needs', :price => 150000)
+Item.create!(:title => 'Galaxy III', :description => 'The next generation mobile', :price => 31000)
+Item.create!(:title => 'Mac book air', :description => 'The lightweight laptop from Apple', :price => 100000)
+Item.create!(:title => 'Dell inspiron', :description => 'The dell laptop', :price => 30000)
+Item.create!(:title => 'Bat', :description => 'good bat', :price => 1500)
+
